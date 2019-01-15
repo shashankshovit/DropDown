c54c28f12a49a019c85a6604dd00db65e6850565
Shashank Shovit <shashankshovit@gmail.com>
2019-01-16 02:04:14 +0530

    Select Box

---
+++ SelectBox.js
@@ -0,0 +1,146 @@
+class SelectBox {
+	constructor(label, choices, options) {
+		this.label = label;
+		this.choices = choices;
+		this.options = Object.assign({}, options);
+		this.validateAttributes();
+		this.html = '';
+	}
+
+	validateAttributes() {
+		let isAcceptable = true;
+		isAcceptable = isAcceptable && (typeof(this.label) == 'string') && (this.label.trim() != '');
+		if(this.choices instanceof Object ) {
+			let values = Object.values(this.choices);
+			isAcceptable = isAcceptable && (values.length != 0)
+				&& (values.every(ch => (typeof(ch) == 'string' || typeof(ch) == 'number')));
+			(this.choices instanceof Array) && this.reorganiseChoices();
+		} else {
+			isAcceptable = false;
+		} 
+		if(!isAcceptable) {
+			let warningMsg = `SelectBox Usage: SelectBox(label, choices, options)
+					label: String
+					choices: Array[<String/Number>]/Object{key: <String/Number>, ...}
+					options: <Object>{
+						className: String
+					}
+				`;
+			throw warningMsg;
+		}
+	}
+
+	reorganiseChoices() {
+		let choices = this.choices;
+		this.choices = {};
+		choices.forEach(ch => {this.choices[ch] = ch;});
+	}
+
+	get optionsContainer() {
+		return this.html.querySelector('.selectBox-options');
+	}
+
+	get arrowContainer() {
+		return this.html.querySelector('.selectBox-arrow');
+	}
+
+	closeDropdown() {
+		this.html.dataset.isOpen = false;
+		this.arrowContainer.classList.remove('down');
+		this.optionsContainer.classList.remove('open');
+	}
+
+	openDropdown() {
+		this.html.dataset.isOpen = true;
+		this.arrowContainer.classList.add('down');
+		this.optionsContainer.classList.add('open');
+	}
+
+	createHTML() {
+		this.html = document.createElement('div');
+		this.html.classList.add('selectBox-push-controller');
+		if(this.options.className) { this.html.classList.add(this.options.className); }
+		this.html.dataset.isOpen = false;
+
+		let labelHolder = document.createElement('div');
+		labelHolder.className = 'selectBox-label';
+		
+		let label = document.createElement('div');
+		label.innerHTML = this.label;
+		labelHolder.appendChild(label);
+		let arrow = document.createElement('div');
+		arrow.className = 'selectBox-arrow left';
+		labelHolder.appendChild(arrow);
+		
+		labelHolder.addEventListener('click', this.pushButtonClicked.bind(this));
+		this.html.appendChild(labelHolder);
+
+		let choices = this.createOptions();
+		this.html.appendChild(choices);
+		return this.html;
+	}
+
+	pushButtonClicked(e) {
+		let isOpen = this.html.dataset.isOpen == 'true';
+		if(isOpen) {
+			this.closeDropdown();
+		} else {
+			this.openDropdown();
+		}
+	}
+
+	createOptions() {
+		let optionsContainer = document.createElement('div');
+		optionsContainer.className = 'selectBox-options';
+
+		let searchInput = document.createElement('input');
+		searchInput.type = 'text';
+		searchInput.placeholder = 'Filter';
+		searchInput.classList.add('selectBox-search');
+		searchInput.addEventListener('keyup', this.filterList.bind(this));
+		optionsContainer.appendChild(searchInput);
+
+		let sortedKeys = Object.keys(this.choices).sort();
+		sortedKeys.forEach(key => {
+			let optionElement = document.createElement('div');
+			optionElement.innerHTML = key;
+			optionElement.dataset.value = this.choices[key];
+			optionElement.addEventListener('click', this.optionSelected.bind(this));
+			optionsContainer.appendChild(optionElement);
+		});
+		return optionsContainer;
+	}
+
+	filterList(e) {
+		let regEx = new RegExp(`.*${e.target.value.split('').join('.*')}.*`);
+		let options = Array.from(this.optionsContainer.children);
+		if(e.target.value.trim() == '') {
+			this.clearFilter();
+		} else {
+			Array.from(this.optionsContainer.children).forEach(option => {
+				if(regEx.test(option.innerText))
+					option.classList.remove('expelled');
+				else
+					option.classList.add('expelled');
+			});
+		}
+	}
+
+	clearFilter() {
+		this.html.querySelector('.selectBox-search').value='';
+		let options = Array.from(this.optionsContainer.children);
+		options.forEach(option => option.classList.remove('expelled'));
+	}
+
+	optionSelected(e) {
+		this.html.firstElementChild.firstElementChild.innerText = e.target.innerText;
+		this.html.dataset.value = e.target.dataset.value;
+		this.clearFilter();
+		this.closeDropdown();
+	}
+
+	render() {
+		let html = this.createHTML();
+		return html;
+	}
+}
